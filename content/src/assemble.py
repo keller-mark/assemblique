abdeb480818066fb2cd9e42c5c3429cbee716876
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2020-01-19 17:09:46 -0500

    Finished ig-to-md script

--- content/src/assemble.py
+++ content/src/assemble.py
@@ -1,5 +1,3 @@
-import boto3
-import botocore
 import sys
 import os
 from os.path import join
@@ -7,8 +5,15 @@ import lzma
 import json
 from pprint import pprint
 
+import boto3
+import botocore
+import frontmatter as fm
+from markdownify import markdownify as md
+
+STATIC_DIR = join('..', 'static')
 INSTA_DIR = join('instagram', 'assemblique')
 ADMIN_USERNAMES = ['assemblique', 'mark_keller_']
+S3_BASE_URL = '//assemblique.s3.us-east-2.amazonaws.com'
 
 HASHTAG_TO_POST_TYPE = {
     '#assembliquewebsite_portfolio': 'portfolio',
@@ -18,9 +23,17 @@ HASHTAG_TO_POST_TYPE = {
     '#assembliquewebsite_forsale': 'forsale'
 }
 
-def s3_file_exists(b3, filepath):
+s3 = boto3.resource(
+    service_name='s3',
+    aws_access_key_id=os.environ['ASSEMBLIQUE_S3_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['ASSEMBLIQUE_S3_SECRET_ACCESS_KEY']
+)
+
+b3 = s3.Bucket("assemblique")
+
+def s3_file_exists(file_key):
     try:
-        b3.Object(filepath).load()
+        b3.Object(file_key).load()
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
             # The object does not exist.
@@ -30,10 +43,28 @@ def s3_file_exists(b3, filepath):
             raise
     return True
 
-
-def post_to_markdown(json_filepath, json_content):
-    shortcode = json_content['node']['shortcode']
+def s3_upload(img_files, year, month):
+    result = []
+    for img_file in img_files:
+        file_key = f'{year}/{month}/{img_file}'
+        if s3_file_exists(file_key):
+            result += [ file_key ]
+        else:
+            data = open(join(INSTA_DIR, img_file), 'rb')
+            obj = b3.put_object(Key=file_key, Body=data)
+            obj.Acl().put(ACL='public-read')
+            result += [ file_key ]
     
+    result = [ f'{S3_BASE_URL}/{key}' for key in result ]
+    return result
+
+def img_urls_to_html(img_urls):
+    result = ""
+    for img_url in img_urls:
+        result += f'<img src="{img_url}" />\n'
+    return result
+
+def post_to_markdown(json_filepath, shortcode, post_types):
     post_files = [ f for f in os.listdir(INSTA_DIR) if f.startswith(json_filepath[:-8]) ]
     txt_file = None
     img_files = []
@@ -44,11 +75,33 @@ def post_to_markdown(json_filepath, json_content):
             img_files += [ post_file ]
     
     if txt_file is not None and len(img_files) > 0:
-        print(txt_file, img_files)
 
         with open(join(INSTA_DIR, txt_file)) as f:
             txt_content = f.read()
-            print(txt_content)
+
+        date = json_filepath[:10]
+        year = date[0:4]
+        month = date[5:7]
+        slug = f"ig-{shortcode}"
+
+        aws_img_urls = s3_upload(img_files, year, month)
+        aws_img_html = img_urls_to_html(aws_img_urls)
+        
+        md_content = md(f'<p>{txt_content}</p>{aws_img_html}<br><a href="https://www.instagram.com/p/{shortcode}/">View on Instagram</a>')
+        post_md = fm.Post(md_content)
+        post_md['instagram_shortcode'] = shortcode
+        post_md['date'] = date
+        post_md['slug'] = slug
+        post_md['title'] = txt_content[:140].replace('\n','')
+        post_md['aws_media_src_url'] = aws_img_urls[0]
+
+        for post_type in post_types:
+            md_filepath = join(STATIC_DIR, post_type, year, month, f"{slug}.md")
+            os.makedirs(os.path.dirname(md_filepath), exist_ok=True)
+            with open(md_filepath, 'wb') as f:
+                fm.dump(post_md, f)
+
+        return post_md
     
     return None
 
@@ -79,23 +132,16 @@ def is_website_post(json_filepath):
                 post_types += [ post_type ]
 
     if len(post_types) == 0:
-        return False, post_types, None
+        return None
     else:
-        return True, post_types, post_to_markdown(json_filepath, json_content)    
+        shortcode = json_content['node']['shortcode']
+        return post_to_markdown(json_filepath, shortcode, post_types)    
         
 
 if __name__ == "__main__":
-    s3 = boto3.resource(
-        service_name='s3',
-        aws_access_key_id=os.environ['ASSEMBLIQUE_S3_ACCESS_KEY_ID'],
-        aws_secret_access_key=os.environ['ASSEMBLIQUE_S3_SECRET_ACCESS_KEY']
-    )
-
-    b3 = s3.Bucket("assemblique")
-
     json_files = [ f for f in os.listdir(INSTA_DIR) if f.endswith("UTC.json.xz") ]
 
     for json_file in json_files:
-        tf, post_types, post_md = is_website_post(json_file)
-        if tf:
-            print(post_types, post_md)
+        post_md = is_website_post(json_file)
+        if post_md is not None:
+            print("Created post", post_md['slug'])
---
+++ content/static/portfolio/2020/01/ig-B7CveYkggo4.md
@@ -0,0 +1,13 @@
+---
+aws_media_src_url: //assemblique.s3.us-east-2.amazonaws.com/2020/01/2020-01-08_03-01-49_UTC.jpg
+date: '2020-01-08'
+instagram_shortcode: B7CveYkggo4
+slug: ig-B7CveYkggo4
+title: New work complete, “Resistance is Futile” made from many many vintage resistors
+  and capacitors, as well as lots of other vintage electronics
+---
+
+New work complete, “Resistance is Futile” made from many many vintage resistors and capacitors, as well as lots of other vintage electronics, including old tube sockets, potentiometers, old soldering irons, camera lenses, and an antique Weston Volt meter. 12”W x 28”H x 6”D, ready to hang on the wall! Message me for pricing! #upcycledart #repurposedart #recycledart #assemblageart #westonvoltmeter #bumblebeeresistors #tubesockets #coppersolderingirons #tvandradioknobs 
+
+![](//assemblique.s3.us-east-2.amazonaws.com/2020/01/2020-01-08_03-01-49_UTC.jpg)   
+[View on Instagram](https://www.instagram.com/p/B7CveYkggo4/)
