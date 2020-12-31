cbc8aa87e188a8e2f6f0a01b32a4850aee63fc13
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2020-12-30 20:58:17 -0500

    Updates

---
+++ _next/static/nQysuD-EXK7iFkJmZABO4/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcommissions","\u002Ffor-sale","\u002Fportfolio","\u002Fsearch","\u002Fpress","\u002F","\u002Fevents","\u002Fcommissions\u002F[slug]","\u002Fevents\u002F[slug]","\u002Fpress\u002F[slug]","\u002Fportfolio\u002F[slug]","\u002Ffor-sale\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
